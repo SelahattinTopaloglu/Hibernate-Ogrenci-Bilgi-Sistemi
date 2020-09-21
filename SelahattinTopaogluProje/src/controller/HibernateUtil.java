09a4ed6763b568cc9f451e2d6ee61119dcc8d48d
Selahattin <39537377+SelahattinTopaloglu@users.noreply.github.com>
2020-09-21 15:27:45 +0300

    Update HibernateUtil.java

--- SelahattinTopaogluProje/src/controller/HibernateUtil.java
+++ SelahattinTopaogluProje/src/controller/HibernateUtil.java
@@ -15,7 +15,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
  * Hibernate Utility class with a convenient method to get Session Factory
  * object.
  *
- * @author asimsinanyuksel
+ *
  */
 public class HibernateUtil {
  
